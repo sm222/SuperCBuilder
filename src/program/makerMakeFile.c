4eb1abae12b9db961fa0d13d2d2d44d6ce2a0500
sm222 <soleil.maraska@gmail.com>
2026-02-11 12:27:32 -0500

    working on makefile

--- src/program/makerMakeFile.c
+++ src/program/makerMakeFile.c
@@ -2,10 +2,11 @@
 
 # include <ctype.h>
 
+static char lastF[MAXPATHLEN];
+
 static void drawVarName(const char* name, const int* fd) {
   size_t i = 0;
-  const size_t nameLen = strlen(name);
-  char outBuffer[(nameLen * 2) + 10];
+  char outBuffer[MAXPATHLEN + 20];
   size_t rptr = 2;
   memcpy(outBuffer, "F_", 2);
   while (name[i]) {
@@ -13,9 +14,18 @@ static void drawVarName(const char* name, const int* fd) {
     i++;
     rptr++;
   }
-  memcpy(outBuffer + rptr, "\t=\t\n", 5);
+  memcpy(lastF, outBuffer, rptr);
+  sprintf(outBuffer + rptr, "\t\t=\t\t%s/\n", name);
   write(*fd, outBuffer, strlen(outBuffer));
 }
+
+ssize_t drawFile(char* name, const int fd) {
+  char outBuffer[MAXPATHLEN + 20];
+  sprintf(outBuffer, "\t\t$(%s)%s\\\n", lastF, name);
+  return write(fd, outBuffer, strlen(outBuffer));
+}
+
+//todo draw file in a object var
 
 static void  buidFileAndFolder(t_node* head, const int* fd) {
   t_node* tmp = head;
@@ -23,6 +33,9 @@ static void  buidFileAndFolder(t_node* head, const int* fd) {
     if (tmp->data.type == folder) {
       drawVarName(tmp->data.name, fd);
       buidFileAndFolder(tmp->child, fd);
+    }
+    else if (tmp->data.type % 2 == 0) {
+      drawFile(tmp->data.name, *fd);
     }
     tmp = tmp->next;
   }
@@ -34,13 +47,25 @@ static ssize_t readList(t_node* head, outFileData* data) {
   return 0;
 }
 
+ssize_t drawName(const char* name, const int fd) {
+  char buff[MAXPATHLEN + 20];
+  memcpy(buff, "NAME\t\t=\t\t", 9);
+  const size_t nlen = strlen(name);
+  memcpy(buff + 9, name, nlen);
+  buff[nlen + 9] = '\n';
+  buff[nlen + 10] = '\n';
+  buff[nlen + 11] = '\0';
+  return write(fd, buff, nlen + 11);
+}
+
 ssize_t buildMakefile(outFileData* data) {
   ssize_t totalBytes = 0;
   const char* hardcodeName = "sm222";//! change later
-  const char* hardcodePname = "scb" ;//!
+  const char* hardcodePname = "scb";//!
   if (!newFile("Makefile", data))
     return -1;
   totalBytes += header(data->fd, "#", hardcodeName, hardcodePname, "Makefile");
+  totalBytes += drawName(hardcodePname, data->fd);
   readList(data->header, data);
   closeFile(data);
   return totalBytes;
--- src/program/makerUtilse.c
+++ src/program/makerUtilse.c
@@ -126,10 +126,11 @@ size_t header(int fd, const char* comment, const char* uName, const char* pName,
   timeinfo = localtime(&rawtime);
   size_t out = 0;
   out += output(fd, "%s - %s - %s\n", comment, comment, comment);
-  out += output(fd, "%s %s Make whit scb on %s\n",comment, fType, asctime(timeinfo));
+  out += output(fd, "%s %s Make whit scb on %s",comment, fType, asctime(timeinfo));
   out += output(fd, "%s build by %s\n", comment, uName);
   out += output(fd, "%s project name -> %s\n", comment, pName);
   out += output(fd, "%s - %s - %s\n", comment, comment, comment);
+  out += output(fd, "\n\n\n\n");
   return out;
 }
 
